1fd49bf4902acf07ddbe85c2dc1ab2e53b50195d
daniel-henrique-calado <danielcalado94@gmail.com>
2024-02-15 18:50:08 -0300

    Adding extract step

---
+++ src/main.py
@@ -0,0 +1,4 @@
+from pipeline import extract
+
+data_frame_list = extract.extract_from_excel("data/input")
+print(data_frame_list)
---
+++ src/pipeline/extract.py
@@ -0,0 +1,30 @@
+import os
+import glob
+
+import pandas as pd
+#import openpyxl
+from typing import List
+
+
+def extract_from_excel(input_path:str) -> List[pd.DataFrame]:
+    """
+    function to read files from a folder "data/input" and return a dataframe lists
+
+    args: input_path(str): folder path with all files
+
+    return: dataframes lists
+    """
+
+    all_files = glob.glob(os.path.join(input_path, "*.xlsx"))
+    df_list = []
+
+    for file in all_files:
+        df_list.append(pd.read_excel(file))
+    
+    return df_list
+
+if __name__ == "__main__":
+    path = "data/input"
+    data_frame_list = extract_from_excel(path)
+    
+    print(data_frame_list)
